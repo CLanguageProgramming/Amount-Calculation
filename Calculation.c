06f1915f7631495b7568594c906a05789ccb1edb
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-08-13 12:51:24 +0530

    Update Calculation.c

--- Calculation.c
+++ Calculation.c
@@ -1,25 +1,21 @@
-//Accept amount and find the denomination of amount.
-//( how many 2000 / 500 / 100 / 50/ 20 / 10  rupees notes make the amount)
-
 #include <stdio.h>
-
 int main()
 {
-    int amount;
-    int final;
-    printf("Enter a amount >>");
-    scanf("%d", &amount);
-    final = amount/10;
-    printf("It takes '%d' 10s to make up %d.\n", final, amount);
-    final = amount/20;
-    printf("It takes '%d' 20s to make up %d.\n", final, amount);
-    final = amount/50;
-    printf("It takes '%d' 50s to make up %d.\n", final, amount);
-    final = amount/100;
-    printf("It takes '%d' 100s to make up %d.\n", final, amount);
-    final = amount/500;
-    printf("It takes '%d' 500s to make up %d.\n", final, amount);
-    final = amount/1000;
-    printf("It takes '%d' 1000s to make up %d.\n", final, amount);
-    return 0;
+    int a;//input from user = 1000
+    int b[10] = {2000, 500, 100, 50, 20, 10, 5, 2, 1};
+    int c;
+    printf("Enter the Amount>> ");
+    scanf("%d", &a);
+    for (int i = 0; i < 10; i++)
+    {
+        c = a/b[i];//2
+        a -= (b[i]*c);
+        printf("%d*%d = %d\n", c, b[i], b[i]*c);
+        if(a == 0)
+        {
+            break;
+        }
+    }
+    getchar();
+    getchar();
 }
