2e0fd419561a391015363aa713cf748ce8f99567
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-08-13 12:58:15 +0530

    Update Calculation.c

--- Calculation.c
+++ Calculation.c
@@ -4,18 +4,37 @@ int main()
     int a;//input from user = 1000
     int b[10] = {2000, 500, 100, 50, 20, 10, 5, 2, 1};
     int c;
+    int d;
+    int e[10];
+    int fe=0;;
     printf("Enter the Amount>> ");
     scanf("%d", &a);
+    d = a;
     for (int i = 0; i < 10; i++)
     {
         c = a/b[i];//2
         a -= (b[i]*c);
+        e[fe] = b[i]*c;
         printf("%d*%d = %d\n", c, b[i], b[i]*c);
         if(a == 0)
         {
             break;
         }
+        fe++;
     }
+    for (int i = 0; i <= fe; i++)
+    {
+        printf("%d ", e[i]);
+        if(i<fe)
+        {
+            printf("+ ");
+        }
+        else
+        {
+            printf("= %d", d);
+        }
+    }
+    
     getchar();
     getchar();
 }
